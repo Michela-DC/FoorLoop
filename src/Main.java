ef2dd34e2f6614ced618fd8dd87bba2d3348b54a
Michela-DC <micheladelconte@outlook.com>
2023-06-15 22:39:11 +0200

    Esercizio foor loop

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+//        Use a for-loop to print all the number from 5 to 25 (including the upper and lower bound)
+        int lowerBound = 5;
+        int upperBound = 25;
+
+        for(int i=lowerBound; i<=upperBound; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
